353230ced604605bede1092a0c099fd20c64f416
AleP1 <alejo.k26@gmail.com>
2024-05-07 01:04:46 -0300

    callbacks 2

--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,26 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+
+   let stringAArray = []
+
+   
+   for (let index = 0; index < arrayOfStrings.length; index++) {
+      const element = arrayOfStrings[index];
+      
+      const splitArrayItem = element.split("")
+      if (splitArrayItem[0] === "a") {
+         stringAArray.push( arrayOfStrings[index] )
+      }
+   }
+
+   console.log( stringAArray )
+
+   return stringAArray
+
+
+
+
 }
 
 module.exports = filter;
